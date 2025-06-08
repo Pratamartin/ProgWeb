eaaadb110e05c6925a62c770bef4dbc972717ddd
Pratamartin <martinhoprata95@gmail.com>
2025-06-08 12:22:59 -0400

    feat(NODE1) : Add node1 exercise

---
+++ NODE/NODE1/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { createLink } = require('./utils');
+require('dotenv-flow').config();
+
+const dir = process.argv[2] || '.';
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const url = decodeURIComponent(req.url);
+
+  if (url === '/' || url === '') {
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        return res.end(`Erro: ${err.message}`);
+      }
+
+      const html = files.map(createLink).join('');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html);
+    });
+
+  } else {
+    const filePath = path.join(dir, url);
+
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        return res.end('Arquivo não encontrado');
+      }
+
+      const html = `
+        <a href="/">Voltar</a><br><br>
+        <pre>${content}</pre>
+      `;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html);
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
