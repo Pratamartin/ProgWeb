99e3825406c50ca5ba3f487282f2e4caf43cf0d2
Pratamartin <martinhoprata95@gmail.com>
2025-06-24 22:37:17 -0400

    feat(ExpTs): Add second exercise

--- EXPTS/src/index.ts
+++ EXPTS/src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { engine } from 'express-handlebars';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/img', express.static(path.join(__dirname, '../public/img')));
+
 app.get('/', (_req, res) => {
   res.send('Hello from TypeScript Express App!');
+});
+
+app.get('/about', (_req, res) => {
+  res.render('about');
 });
 
 app.listen(PORT, () => {
