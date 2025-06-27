b2143cece80b1d17355794cc20634091bf60f966
Pratamartin <martinhoprata95@gmail.com>
2025-06-26 22:40:29 -0400

    feat(ExpTs): Add seventh exercise

--- EXPTS/src/router/router.ts
+++ EXPTS/src/router/router.ts
@@ -4,28 +4,28 @@ import { LoremIpsum } from 'lorem-ipsum';
 const router = Router();
 
 const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
+  sentencesPerParagraph: { min: 4, max: 8 },
+  wordsPerSentence: { min: 4, max: 16 }
 });
+
 
 router.get('/', (_req, res) => {
   res.send('Hello from TypeScript Express App!');
 });
 
+
 router.get('/about', (_req, res) => {
   res.render('about');
 });
 
+
 router.get('/lorem/:count', (req, res) => {
   const count = parseInt(req.params.count);
+
   if (isNaN(count) || count < 1) {
-    return res.status(400).send('<h1>Parâmetro inválido. Use um número inteiro positivo.</h1>');
+    return res
+      .status(400)
+      .send('<h1>Parâmetro inválido. Use um número inteiro positivo.</h1>');
   }
 
   const paragraphs = lorem.generateParagraphs(count);
@@ -36,5 +36,34 @@ router.get('/lorem/:count', (req, res) => {
 
   res.send(`<html><body>${html}</body></html>`);
 });
+
+
+router.get('/hb1', (_req, res) => {
+  res.render('hb/hb1', {
+    mensagem: 'Olá, você está aprendendo Express + HBS!'
+  });
+});
+
+
+router.get('/hb2', (_req, res) => {
+  res.render('hb/hb2', {
+    poweredByNodejs: true,
+    name: 'Express',
+    type: 'Framework'
+  });
+});
+
+
+router.get('/hb3', (_req, res) => {
+  const professores = [
+    { nome: 'David Fernandes', sala: 1238 },
+    { nome: 'Horácio Fernandes', sala: 1233 },
+    { nome: 'Edleno Moura', sala: 1236 },
+    { nome: 'Elaine Harada', sala: 1231 }
+  ];
+
+  res.render('hb/hb3', { professores });
+});
+
 
 export default router;
